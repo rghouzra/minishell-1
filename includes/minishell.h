c1daf9be4b4e23a39fe9fed4a25bc28985339ac0
4yuub <karafi.work@gmail.com>
2022-01-23 14:40:41 +0100

    init header files and minishell.c

---
+++ includes/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 22:13:40 by ayoub             #+#    #+#             */
+/*   Updated: 2022/01/23 13:54:52 by ayoub            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "colors.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
