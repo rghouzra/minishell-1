b3d5670bfc805eb284a93ca830c125f9948c059e
4yuub <karafi.work@gmail.com>
2022-01-27 23:05:08 +0100

    add redirections to tokenizer

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:08:09 by ayoub             #+#    #+#             */
-/*   Updated: 2022/01/27 20:16:20 by ayoub            ###   ########.fr       */
+/*   Updated: 2022/01/27 23:04:42 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,39 @@ static int	is_space(char c)
 		|| c == '\f' || c == '\r' || c == ' ');
 }
 
+static bool	is_redirection(char *s)
+{
+	return ((*s == '<' && *(s + 1) == '<' && *(s + 2) != '<') \
+			|| (*s == '>' && *(s + 1) == '>' && *(s + 2) != '>') \
+			|| (*s == '<' && *(s + 1) != '<') \
+			|| (*s == '>' && *(s + 1) != '>')
+	);
+}
+
 static t_list	*quote_error(void)
 {
 	printf("syntax error: missing to close quotes\n");
 	return (NULL);
 }
 
-static char	*handle_semicolon_pipe(char *s, t_list **tokens, t_gc **garbage)
+static char	*special(char *s, t_list **tokens, t_gc **garbage)
 {
 	t_list	*last_token;
+	int		n;
 
 	last_token = ft_lstlast(*tokens);
+	n = 1;
+	if ((*s == '>' && *(s + 1) == '>' && *(s + 2) != '>') \
+		|| (*s == '<' && *(s + 1) == '<' && *(s + 2) != '<'))
+		n = 2;
 	if (last_token->content)
 		ft_lstadd_back(tokens, \
-			collect(ft_lstnew(collect(ft_substr(s, 0, 1), garbage)), garbage));
+			collect(ft_lstnew(collect(ft_substr(s, 0, n), garbage)), garbage));
 	else
 		last_token->content = \
 			collect(append_char(last_token->content, *s), garbage);
 	ft_lstadd_back(tokens, collect(ft_lstnew(NULL), garbage));
-	s++;
+	s += n;
 	return (s);
 }
 
@@ -50,8 +64,8 @@ t_list	*tokenize(char *s, t_gc **garbage)
 	{
 		if ((!tokens || is_space(*s)) && (*(s + 1) && !is_space(*(s + 1))))
 			ft_lstadd_back(&tokens, collect(ft_lstnew(NULL), garbage));
-		else if (*s == ';' || *s == '|')
-			s = handle_semicolon_pipe(s, &tokens, garbage);
+		else if (*s == ';' || *s == '|' || is_redirection(s))
+			s = special(s, &tokens, garbage);
 		if (*s == '\'' || *s == '"')
 		{
 			s = handle_quotes(s, &tokens);
