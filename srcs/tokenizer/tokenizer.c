43d4a2f97802b3c4d259829da589295da20560df
4yuub <karafi.work@gmail.com>
2022-01-28 15:41:38 +0100

    fix: error in redirection

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:08:09 by ayoub             #+#    #+#             */
-/*   Updated: 2022/01/27 23:18:56 by ayoub            ###   ########.fr       */
+/*   Updated: 2022/01/28 15:41:13 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,9 @@ static int	is_space(char c)
 
 static bool	is_redirection(char *s)
 {
-	return ((*s == '<' && *(s + 1) == '<' && *(s + 2) != '<') \
-			|| (*s == '>' && *(s + 1) == '>' && *(s + 2) != '>') \
-			|| (*s == '<' && *(s + 1) != '<') \
-			|| (*s == '>' && *(s + 1) != '>')
+	return ((*s == '<' && *(s + 1) == '<') \
+			|| (*s == '>' && *(s + 1) == '>') \
+			|| (*s == '<') || (*s == '>')
 	);
 }
 
@@ -37,18 +36,22 @@ static char	*special(char *s, t_list **tokens, t_gc **garbage)
 {
 	t_list	*last_token;
 	int		n;
+	int		i;
 
 	last_token = ft_lstlast(*tokens);
 	n = 1;
-	if ((*s == '>' && *(s + 1) == '>' && *(s + 2) != '>') \
-		|| (*s == '<' && *(s + 1) == '<' && *(s + 2) != '<'))
+	if ((*s == '>' && *(s + 1) == '>') || (*s == '<' && *(s + 1) == '<'))
 		n = 2;
 	if (last_token->content)
 		ft_lstadd_back(tokens, \
 			collect(ft_lstnew(collect(ft_substr(s, 0, n), garbage)), garbage));
 	else
-		last_token->content = \
-			collect(append_char(last_token->content, *s), garbage);
+	{
+		i = -1;
+		while (++i < n)
+			last_token->content = \
+				collect(append_char(last_token->content, *(s + i)), garbage);
+	}
 	ft_lstadd_back(tokens, collect(ft_lstnew(NULL), garbage));
 	s += n;
 	return (s);
@@ -64,7 +67,7 @@ t_list	*tokenize(char *s, t_gc **garbage)
 	{
 		if ((!tokens || is_space(*s)) && (*(s + 1) && !is_space(*(s + 1))))
 			ft_lstadd_back(&tokens, collect(ft_lstnew(NULL), garbage));
-		else if (*s == ';' || *s == '|' || is_redirection(s))
+		while (*s == ';' || *s == '|' || is_redirection(s))
 			s = special(s, &tokens, garbage);
 		if (*s == '\'' || *s == '"')
 		{
