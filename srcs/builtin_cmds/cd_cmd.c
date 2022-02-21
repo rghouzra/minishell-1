9691b85672c7e5826fb378c2f6af2f1121475072
Anass Klaikel <88429076+Aklaikel@users.noreply.github.com>
2022-02-21 02:38:18 +0100

    add the cd builtin command

--- srcs/builtin_cmds/cd_cmd.c
+++ srcs/builtin_cmds/cd_cmd.c
@@ -1,33 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd_cmd.c                                          :+:      :+:    :+:   */
+/*   cd_cmd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aklaikel <aklaikel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/20 17:18:06 by aklaikel          #+#    #+#             */
-/*   Updated: 2022/02/21 03:09:36 by aklaikel         ###   ########.fr       */
+/*   Created: 2022/02/20 20:10:02 by aklaikel          #+#    #+#             */
+/*   Updated: 2022/02/21 02:37:39 by aklaikel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	pwd_cmd(char **cmd_list)
+void	cd_cmd(char **cmd_list)
 {
-	char	cwd[1024];
+	char	**cwd;
+	int		ret;
 
-	if (!cmd_list || !*cmd_list || ft_strncmp(cmd_list[0], "pwd", 255))
+	if (!cmd_list || !*cmd_list || ft_strncmp(cmd_list[0], "cd", 255))
 		return ;
 	if (cmd_list[1] && cmd_list[1][0] == '-' \
-		&& cmd_list[1][1] != '\0' && cmd_list[1][1] != '-')
+			&& cmd_list[1][1] != '\0' && cmd_list[1][1] != '-')
 	{
 		g_tools.exit_status = 1;
 		printf("%s: illegal option -- %c\n", cmd_list[0], cmd_list[1][1]);
-		printf("usage: env [with no options]\n");
+		printf("usage: cd [with no options]\n");
 		return ;
 	}
-	if (getcwd(cwd, 1024))
-		printf("%s\n", cwd);
+	if (!cmd_list[1])
+		ret = chdir(getenv("HOME"));
 	else
-		perror("pwd:");
+		ret = chdir(cmd_list[1]);
+	if (ret < 0)
+	{
+		perror("cd");
+		g_tools.exit_status = 1;
+	}
 }
