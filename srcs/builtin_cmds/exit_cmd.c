5a2cdb74dee73e11d462b922dd71c3e5554c1987
Anass Klaikel <88429076+Aklaikel@users.noreply.github.com>
2022-02-22 04:32:21 +0100

    add the exit builtin command

---
+++ srcs/builtin_cmds/exit_cmd.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_cmd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aklaikel <aklaikel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 02:11:16 by aklaikel          #+#    #+#             */
+/*   Updated: 2022/02/22 04:28:50 by aklaikel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	get_nbr(const char *str)
+{
+	int		i;
+	int		n;
+	long	result;
+
+	i = 0;
+	n = 1;
+	result = 0;
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i++] == '-')
+			n = -1;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i++] - '0';
+	}
+	while (str[i] == ' ')
+		i++;
+	if (str[i])
+		return (printf("exit: %s: numeric argument required\n", str), \
+			exit(255), 255);
+	return (result * n);
+}
+
+void	exit_cmd(char **cmd_list, t_gc **garbage)
+{
+	int	status;
+
+	if (!cmd_list || !*cmd_list || ft_strncmp(*cmd_list, "exit", 255))
+		return ;
+	clear(garbage);
+	if (!cmd_list[1])
+		exit(0);
+	status = get_nbr(cmd_list[1]);
+	if (cmd_list[2])
+	{
+		g_tools.exit_status = 1;
+		printf("bash: exit: too many arguments");
+	}
+	else
+		exit(status);
+}
