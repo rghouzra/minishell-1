60c2d08794db450660c679a1f0154a1b67b3d169
Anass Klaikel <88429076+Aklaikel@users.noreply.github.com>
2022-02-27 15:48:43 +0100

    get home from the virtual env instead of env

--- srcs/builtin_cmds/cd_cmd.c
+++ srcs/builtin_cmds/cd_cmd.c
@@ -3,31 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   cd_cmd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aklaikel <aklaikel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 20:10:02 by aklaikel          #+#    #+#             */
-/*   Updated: 2022/02/22 17:51:43 by ayoub            ###   ########.fr       */
+/*   Updated: 2022/02/27 15:43:21 by aklaikel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cd_cmd(char **cmd_list)
+static char	*cd_getenv(char *name, t_var *env)
+{
+	t_var	*var;
+	int		max;
+
+	var = env;
+	while (var)
+	{
+		max = ft_strlen(var->name);
+		if ((int) ft_strlen(name) > max)
+			max = ft_strlen(name);
+		if (!ft_strncmp(var->name, name, max))
+			return (var->value);
+		var = var->next;
+	}
+	return (NULL);
+}
+
+static void	ft_error(char **cmd_list)
+{
+	g_tools.exit_status = 1;
+	printf("%s: illegal option -- %c\n", cmd_list[0], cmd_list[1][1]);
+	printf("usage: cd [with no options]\n");
+	return ;
+}
+
+static void	ft_error_home(void)
+{
+	g_tools.exit_status = 1;
+	printf("minishell: cd: HOME not set");
+}
+
+void	cd_cmd(char **cmd_list, t_var *env)
 {
 	int		ret;
+	char	*path;
 
 	if (!cmd_list || !*cmd_list || ft_strncmp(cmd_list[0], "cd", 255))
 		return ;
 	if (cmd_list[1] && cmd_list[1][0] == '-' \
 			&& cmd_list[1][1] != '\0' && cmd_list[1][1] != '-')
-	{
-		g_tools.exit_status = 1;
-		printf("%s: illegal option -- %c\n", cmd_list[0], cmd_list[1][1]);
-		printf("usage: cd [with no options]\n");
-		return ;
-	}
+		ft_error(cmd_list);
 	if (!cmd_list[1])
-		ret = chdir(getenv("HOME"));
+	{
+		path = cd_getenv("HOME", env);
+		if (!path)
+			ft_error_home();
+		ret = chdir(path);
+	}
 	else
 		ret = chdir(cmd_list[1]);
 	if (ret < 0)
