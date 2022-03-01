6adb57c29c4aaddfb5245bcb74326df347e1f652
4yuub <karafi.work@gmail.com>
2022-03-02 00:24:25 +0100

    fix: error on command is empty string

--- srcs/execution/get_path.c
+++ srcs/execution/get_path.c
@@ -6,7 +6,7 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 23:48:58 by aklaikel          #+#    #+#             */
-/*   Updated: 2022/03/01 22:56:54 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/03/02 00:23:47 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*get_path(char *word, t_var *env, t_gc **garbage)
 	int		i;
 
 	i = 0;
-	if (!word || !env)
+	if (!word || !*word || !env)
 		return (NULL);
 	if (access(word, X_OK) == 0)
 		return (word);
