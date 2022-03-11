2a965b20bd04f61030caf240613bfc4015270522
4yuub <karafi.work@gmail.com>
2022-03-11 14:49:03 +0100

    fix: return empty string if the value of variable is null (exported but not have value)

--- srcs/env/env.c
+++ srcs/env/env.c
@@ -6,7 +6,7 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 20:57:17 by akarafi           #+#    #+#             */
-/*   Updated: 2022/02/10 16:48:13 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/03/11 14:47:12 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ char	*ft_getenv(char *name, t_var *env)
 		max = ft_strlen(var->name);
 		if ((int) ft_strlen(name) > max)
 			max = ft_strlen(name);
-		if (!ft_strncmp(var->name, name, max))
+		if (!ft_strncmp(var->name, name, max) && var->value)
 			return (var->value);
 		var = var->next;
 	}
